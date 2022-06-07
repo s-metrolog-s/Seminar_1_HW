5fa2681eaeab883b0edbcbd5dfc03817b9200500
Sergey <metrolog_omsk@mail.ru>
2022-06-07 22:05:52 +0500

    Решил дополнительную задачу № 1

---
+++ Extra_Task_1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 1. Пользователь вводит число N (N > 0). Программа должна вывести N единиц на экран. 
+// N = 4 -> 1, 1, 1, 1
+// N = 2 -> 1, 1
+
+Console.Clear();
+Console.WriteLine("***************************************");
+Console.WriteLine("Выводим количество единиц по числу N");
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int numbers = 1;
+
+if (N > 0)
+{
+    while (numbers < N)
+    {
+        Console.Write("1, ");
+        numbers++;
+    }
+        
+    if (numbers == N) // Добавил условие, чтобы убрать знак "," в конце строки
+    {
+        Console.Write("1");
+    }
+
+    Console.WriteLine("");
+}
+else
+{
+    Console.WriteLine("Введите число больше 0");
+}
+Console.WriteLine("***************************************");
