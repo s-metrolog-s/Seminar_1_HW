519c7e58ebd3588e79c21b9c6ddc13c3bb02ed9c
Sergey <metrolog_omsk@mail.ru>
2022-06-08 00:36:20 +0500

    Дополнил решение дополнительной задачи № 2

--- Extra_Task_2/Program.cs
+++ Extra_Task_2/Program.cs
@@ -1,8 +1,23 @@
 ﻿// Задача 2. Даны два числа a, b, такие что a < b. 
 // Вывести на экран сколько раз число a поместиться в числе b.
 
-int a = 3;
-int b = 13;
-
 Console.Clear();
-Console.WriteLine(b / a);
+Console.WriteLine("***************************************");
+Console.WriteLine("Сколько раз а поместиться в число b");
+
+Console.Write("Введите число a: "); //Запрашиваем первое число
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b: "); //Запрашиваем второе число
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (b < a) //Проверяем выполение условия задачи a < b
+{
+    Console.Write("Введеное b, меньше a, не соответствует условию задачи");
+}
+else
+{
+Console.Write("Число a поместится в b ");
+Console.Write(b / a);
+Console.WriteLine(" раз/a");
+Console.WriteLine("***************************************");
+}
