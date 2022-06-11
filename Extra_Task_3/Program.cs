3e499048adf932cc373a9e4edab8ea03039db699
Sergey <metrolog_omsk@mail.ru>
2022-06-11 11:11:04 +0500

    Дополнительная задача № 3 (Добавил комментарии)

--- Extra_Task_3/Program.cs
+++ Extra_Task_3/Program.cs
@@ -1,13 +1,17 @@
 ﻿// Задача 3. На ввод подаётся рост трёх спортсменов. 
 //           Расположить их от большего к меньшему.
 
-//double s1 = 1.78;
-//double s2 = 1.65;
-//double s3 = 1.83;
-
-double s1 = 1.35;
-double s2 = 1.85;
+double s1 = 1.78;
+double s2 = 1.65;
 double s3 = 1.83;
+
+Console.Clear();
+Console.Write("Рост первого спортсмена: ");
+Console.WriteLine(s1);
+Console.Write("Рост второго спортсмена: ");
+Console.WriteLine(s2);
+Console.Write("Рост третьего спортсмена: ");
+Console.WriteLine(s3);
 
 double max = s1;
 double middle = s1;
@@ -22,6 +26,7 @@ if (s3 < min) min = s3;
 if ((s2 < max) && (s2 > min)) middle = s2;
 if ((s3 < max) && (s3 > min)) middle = s3;
 
+Console.WriteLine("Выстроим по убыванию:");
 Console.WriteLine(max);
 Console.WriteLine(middle);
 Console.WriteLine(min);
