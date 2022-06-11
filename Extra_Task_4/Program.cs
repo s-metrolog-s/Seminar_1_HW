8b064cad0c7c7978cf1e015cbb23be5728f7729e
Sergey <metrolog_omsk@mail.ru>
2022-06-11 11:23:54 +0500

    Дополнительная задача № 4 (Добавил Random и проверку знаков)

--- Extra_Task_4/Program.cs
+++ Extra_Task_4/Program.cs
@@ -1,7 +1,8 @@
 ﻿// Задача 4. Дано N секунд. Вывести время в формате часы:минуты:секунды.
 // N = 72334 -> 20:5:34 
 
-int N = 72334;
+// int N = 72334;
+int N = new Random().Next(1, 100000);
 int hour;
 int min;
 int sec;
@@ -16,9 +17,38 @@ hour = N / 3600; //Считаем целое киолчество часов
 min = (N - (hour * 3600)) / 60; //Считаем целое киолчество минут
 sec = (N - hour * 3600 - min * 60); //Оставшееся присваиваем секундам
 
-Console.Write(hour);
-Console.Write(":");
-Console.Write(min);
-Console.Write(":");
-Console.WriteLine(sec);
+if (hour < 10) //Добавляем 0, если получается значение из одного знака
+{
+    Console.Write(0);
+    Console.Write(hour);
+    Console.Write(":");
+}
+else
+{
+    Console.Write(hour);
+    Console.Write(":");
+}
+
+if (min < 10) //Добавляем 0, если получается значение из одного знака
+{
+    Console.Write(0);
+    Console.Write(min);
+    Console.Write(":");
+}
+else
+{
+    Console.Write(min);
+    Console.Write(":");
+}
+
+if (sec < 10)
+{
+    Console.Write(0);
+    Console.WriteLine(sec);
+}
+else
+{
+    Console.WriteLine(sec);
+}
+
 Console.WriteLine("***************************************");
